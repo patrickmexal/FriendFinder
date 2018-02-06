7202970680b28956a32f31f9691963c83351cc14
Patrick Mexal <patrickmexal@gmail.com>
2018-02-05 18:12:12 -0700

    update four

--- server.js
+++ server.js
@@ -5,20 +5,16 @@ var bodyParser = require("body-parser")
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-var jsonParser = bodyParser.json()
- 
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
- 
-// POST /login gets urlencoded bodies
-app.post('/login', urlencodedParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400)
-  res.send('welcome, ' + req.body.username)
-})
- 
-// POST /api/users gets JSON bodies
-app.post('/api/users', jsonParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400)
-  // create user in req.body
-}) 
+
 app.listen(3000)
+
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.use(bodyParser.json())
+
+app.use(function (req, res) {
+	res.setHeader("content-type", "text-plain")
+	res.write("you posted:\n")
+	res.end(JSON.stringify(req.body, null, 2))
+})
+
