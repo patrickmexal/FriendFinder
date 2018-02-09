9c2a27cce06e8d2d0b405c69846083eb5b9ab1b9
Patrick Mexal <patrickmexal@gmail.com>
2018-02-09 15:48:35 -0700

    update fifteen

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,7 @@
+var friendData = require("data/friends.js");
+
+module.exports = function(app) {
+	app.get("data/friends.js", function(req, res) {
+		res.json(friendData);
+	})
+};
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
 require("/../routing/htmlRoutes.js")(app);
+require("/../routing/apiRoutes.js")(app);
 
 app.listen(PORT, function() {
 	console.log("App listening on port: " + PORT);
