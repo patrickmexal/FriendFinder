0bb950914cbd1a1507955c09282a506a4fe3a6bd
Patrick Mexal <patrickmexal@gmail.com>
2018-02-09 12:31:01 -0700

    update twelve

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var theFriendArray = [
+	{
+		"name": "Bob Hope",
+		"picture": "#",
+		"scores": [5, 2, 3, 1, 4, 3, 5, 5, 3, 2]
+	},
+	{
+		"name": "Chris Brown",
+		"picture": "#",
+		"scores": [4, 4, 4, 2, 4, 2, 1, 5, 2, 2]
+	},
+	{
+		"name": "Oprah Winfrey",
+		"picture": "#",
+		"scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	},
+	{
+		"name": "Lance Armstrong",
+		"picture": "#",
+		"scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+	},
+	{
+		"name": "Buckethead",
+		"picture": "#",
+		"scores": [1, 5, 1, 5, 1, 5, 1, 5, 1, 5]
+	}, 
+	{
+		"name": "Celine Dion",
+		"picture": "#",
+		"scores": [2, 3, 2, 2, 3, 2, 3, 3, 3, 3]
+	}, 
+	{
+		"name": "Kanye West",
+		"picture": "#",
+		"scores": [1 ,2, 2, 1, 2, 2, 2, 2, 2, 2]
+	},
+	{
+		"name": "Chewbacca",
+		"picture": "#",
+		"scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},
+	{
+		"name": "Latrell Sprewell",
+		"picture": "#",
+		"scores": [2, 2, 4, 4, 2, 2, 5, 5, 5, 5]
+	},
+	{
+		"name": "Bing Crosby",
+		"picture": "#",
+		"scores": [1, 5, 1, 5, 1, 5, 1, 5, 1, 5]
+	}
+	];	
+	 module.exports = theFriendArray;
+	 console.log(theFriendArray);
