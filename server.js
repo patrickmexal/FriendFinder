0d546eaeee96db353d896ff535590ce46e62fbb7
Patrick Mexal <patrickmexal@gmail.com>
2018-02-09 14:00:41 -0700

    update fifteen

--- server.js
+++ server.js
@@ -4,19 +4,13 @@ var bodyParser = require("body-parser")
 var path = require("path");
 var PORT = process.env.PORT || 3000;
  
-app.get('/', function (req, res) {
-  res.send("Welcome to FriendFinder!")
-})
 
 app.use(bodyParser.urlencoded({ extended: false}))
 
 app.use(bodyParser.json())
 
-app.use(function (req, res) {
-	res.setHeader("content-type", "text-plain")
-	res.write("you posted:\n")
-	res.end(JSON.stringify(req.body, null, 2))
-})
+require("/../routing/htmlRoutes.js")(app);
+
 app.listen(PORT, function() {
 	console.log("App listening on port: " + PORT);
 })
