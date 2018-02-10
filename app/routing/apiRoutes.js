7bf23285eeeb756ab75a0e1ceef90143cceddcab
Patrick Mexal <patrickmexal@gmail.com>
2018-02-10 10:00:09 -0700

    update twenty

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,8 @@
-var friendData = require("data/friends.js");
+var friendData = require("/data/friends.js");
 
 module.exports = function(app) {
 	app.get("/data/friends.js", function(req, res) {
 		res.json(friendData);
 	})
 }
+console.log(friendData);
